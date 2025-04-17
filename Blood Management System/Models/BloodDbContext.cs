14ce52f1a18f8454470dc9eb6c38128db0e8ef9a
ganesh <ganesh@GANESH>
2025-04-17 14:43:02 +0530

    Add project files.

---
+++ Blood Management System/Models/BloodDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Blood_Management_System.Models;
+using Microsoft.EntityFrameworkCore;
+namespace BloodDonor.Models
+{
+    public class BloodDbContext : DbContext
+    {
+        public BloodDbContext(DbContextOptions<BloodDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Blood> Bloods { get; set; }
+
+    }
+}
